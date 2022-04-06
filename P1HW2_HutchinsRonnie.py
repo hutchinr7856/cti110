880545bc990fe939711732c7a52845cef7a612b7
hutchinr7856 <102985940+hutchinr7856@users.noreply.github.com>
2022-04-06 04:54:01 -0400

    Add files via upload
    Fourth Submission

---
+++ P1HW2_HutchinsRonnie.py
@@ -0,0 +1,22 @@
+#This a comment.
+print('Basic input/output project')
+#This is a comment.
+print('04/05/2022')
+#This is a comment.
+print('CTI-110 P1HW2 - Pizza Order')
+#This a comment.
+print('HutchinsRonnie')
+#This a comment.
+print('Pseudocode')
+
+
+
+print('How many students do you want to order pizza for?', 26)
+
+print('   Pizza Order   ')
+print('Number of Students :', 26)
+print('Pizza Slices Needed:', 78)
+print('Pizza Needed :', 13)
+
+
+
