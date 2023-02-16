cb1609551a832006d809da22cc5de47d6a315536
hutchinr7856 <102985940+hutchinr7856@users.noreply.github.com>
2023-02-16 13:48:17 -0500

    Add files via upload

--- P1HW2_HutchinsRonnie.py
+++ P1HW2_HutchinsRonnie.py
@@ -1,22 +1,23 @@
-#This a comment.
-print('Basic input/output project')
-#This is a comment.
-print('04/05/2022')
-#This is a comment.
-print('CTI-110 P1HW2 - Pizza Order')
-#This a comment.
-print('HutchinsRonnie')
-#This a comment.
-print('Pseudocode')
+# This program calculates and display travel expenses
+#2-16-2023
+#CTI-110 P1HW2-Travel Expense
+#Ronnie Hutchins
 
 
 
-print('How many students do you want to order pizza for?', 26)
 
-print('   Pizza Order   ')
-print('Number of Students :', 26)
-print('Pizza Slices Needed:', 78)
-print('Pizza Needed :', 13)
-
-
-
+print("This program calculates and display travel expenses")
+print()
+budget = float(input("Enter budget: "))
+print()
+name = input("Enter your travel destination: ")
+print()
+gas = float(input("How much do you think you will spend on gas? "))
+print()
+hotel=float(input("How much will you need for hotel? "))
+print()
+food=float(input("How much do you need for food? "))
+print()
+balance = budget - gas - hotel - food
+print("Remaining Balance:", balance)
+print()
